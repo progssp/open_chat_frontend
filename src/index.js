93fe1dd1b5629f36a58163ed090b5430e4c24ad6
progssp <shantanusingh922@gmail.com>
2026-04-18 13:45:09 +0530

    changes for vercel deploy

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Login from './components/Login/Login';
-// import Test from './components/Test';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Login from "./components/Login/Login";
 
 const router = createBrowserRouter([
-  
   {
-    path: '/app',
-    element: <Login/>
+    path: "/app",
+    element: <Login />,
   },
   {
-    path: '/app/chat',
-    element: <App/>
-  }
+    path: "/app/chat",
+    element: <App />,
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
